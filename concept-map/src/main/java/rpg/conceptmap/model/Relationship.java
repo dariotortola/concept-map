08b110041da8f6f914e4e568c9b8199fdc257356
Dario Tortola <dariotortola@gmail.com>
2016-07-13 10:11:09 +0200

    First commit

---
+++ concept-map/src/main/java/rpg/conceptmap/model/Relationship.java
@@ -0,0 +1,19 @@
+package rpg.conceptmap.model;
+
+/**
+ * Relación entre varios conceptos. 
+ * 
+ * @author dtortola
+ *
+ */
+public class Relationship {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
